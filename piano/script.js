a2a04c3855381765d0f7b565cffd9039b5ff7dae
wangjiayi <wjiayi01@gmail.com>
2018-02-21 13:36:56 -0800

    change query
    change query

--- piano/script.js
+++ piano/script.js
@@ -1,4 +1,30 @@
-//get context 
+'use strict'
+// whitekeybutton[0].addEventListener('onclick',function(){
+//     whitekeybutton.style.backgroundColor = "green";
+// });
+$(document).ready(function(){
+    // $("#fright").click(function(){
+    //     alert("tryeyryyey");
+    // });
+    $(".whitekeybutton").on( "click", function(){
+        $(this).css('background-color', 'gray');
+        var time = 200;
+        setTimeout(function() {
+            $('.whitekeybutton').css("background-color", "white");
+        }, time);
+    });
+    $(".blackbutton").on( "click", function(){
+        $(this).css('background-color', 'gray');
+        var time = 200;
+        setTimeout(function() {
+            $('.blackbutton').css("background-color", "black");
+        }, time);
+    });
+    // $('#keyboard').click(function(){
+
+    //     alert('afwe');
+    // });
+});//get context 
 (function() {
     'use strict'
     var audio_con;
