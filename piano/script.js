d48900c8fc2215fd5f90763648678213046c5231
wangjiayi <wjiayi01@gmail.com>
2018-02-21 13:45:39 -0800

    piano

--- piano/script.js
+++ piano/script.js
@@ -1,30 +1,4 @@
-'use strict'
-// whitekeybutton[0].addEventListener('onclick',function(){
-//     whitekeybutton.style.backgroundColor = "green";
-// });
-$(document).ready(function(){
-    // $("#fright").click(function(){
-    //     alert("tryeyryyey");
-    // });
-    $(".whitekeybutton").on( "click", function(){
-        $(this).css('background-color', 'gray');
-        var time = 200;
-        setTimeout(function() {
-            $('.whitekeybutton').css("background-color", "white");
-        }, time);
-    });
-    $(".blackbutton").on( "click", function(){
-        $(this).css('background-color', 'gray');
-        var time = 200;
-        setTimeout(function() {
-            $('.blackbutton').css("background-color", "black");
-        }, time);
-    });
-    // $('#keyboard').click(function(){
-
-    //     alert('afwe');
-    // });
-});//get context 
+//get context 
 (function() {
     'use strict'
     var audio_con;
